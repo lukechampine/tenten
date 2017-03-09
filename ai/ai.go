9d23b65f6252ad656028e6d4b239dab4febc7f1f
lukechampine <luke.champine@gmail.com>
2017-03-09 18:15:19 -0500

    optimize using bit packing

--- ai/ai.go
+++ ai/ai.go
@@ -2,102 +2,146 @@ package ai
 
 import "github.com/lukechampine/tenten/game"
 
-func Heuristic(b *game.Board) int {
-	// maximize empty lines
-	var emptyLines int
-	for i := range b {
-		// check row i
-		clear := true
-		for x := range b {
-			if b[x][i] != game.Empty {
-				clear = false
-				break
+var popcounts [256]byte
+var stride3Lookup [1 << 10]int
+var stride5Lookup [1 << 10]int
+
+func init() {
+	for i := range popcounts {
+		var n byte
+		for x := i; x != 0; x >>= 1 {
+			if x&1 != 0 {
+				n++
 			}
 		}
-		if clear {
-			emptyLines++
-		}
-		// check column i
-		clear = true
-		for y := range b {
-			if b[i][y] != game.Empty {
-				clear = false
-				break
-			}
-		}
-		if clear {
-			emptyLines++
-		}
+		popcounts[i] = n
 	}
 
-	// maximize contiguity
-	var contiguous, disparate int
-	for i := 1; i < 10; i++ {
-		for j := 1; j < 10; j++ {
-			if b[i][j] != game.Empty {
-				if b[i-1][j] != game.Empty && b[i][j-1] != game.Empty {
-					contiguous += 4
-				} else if b[i-1][j] != game.Empty || b[i][j-1] != game.Empty {
-					contiguous++
-				} else {
-					disparate++
+	for u := range stride3Lookup {
+		for i := uint16(0); i < 10; i++ {
+			l := 0
+			for ; i < 10 && (u&(1<<i) == 0); i++ {
+				l++
+				if l >= 3 {
+					stride3Lookup[u]++
+				}
+				if l >= 5 {
+					stride5Lookup[u]++
 				}
 			}
+		}
+	}
+}
 
+func popcount(x uint16) (n byte) {
+	return popcounts[byte(x)] + popcounts[byte(x>>8)]
+}
+
+const fullLine = (uint16(1) << 10) - 1
+
+type hboard struct {
+	origrows [10]uint16
+	origcols [10]uint16
+	rows     [10]uint16
+	cols     [10]uint16
+}
+
+func (h *hboard) place(p game.Piece, x, y int) bool {
+	h.rows, h.cols = h.origrows, h.origcols
+	fullRows, fullCols := make([]byte, 0, 10), make([]byte, 0, 10)
+	for _, d := range p.Dots() {
+		// TODO: convert dots to row bits and use bits&row > 0 to check for collisions
+		dx, dy := uint16(d.X+x), uint16(d.Y+y)
+		if h.rows[dy]&(1<<dx) != 0 {
+			return false
+		}
+		h.rows[dy] |= 1 << dx
+		if h.rows[dy] == fullLine {
+			fullRows = append(fullRows, byte(dy))
+		}
+		if h.cols[dx]&(1<<dy) != 0 {
+			return false
+		}
+		h.cols[dx] |= 1 << dy
+		if h.cols[dx] == fullLine {
+			fullCols = append(fullCols, byte(dx))
+		}
+	}
+	for _, r := range fullRows {
+		h.rows[r] = 0
+		for i := range h.cols {
+			h.cols[i] &^= 1 << uint16(r)
+		}
+	}
+	for _, c := range fullCols {
+		h.cols[c] = 0
+		for i := range h.rows {
+			h.rows[i] &^= 1 << uint16(c)
+		}
+	}
+	return true
+}
+
+func makeHBoard(b *game.Board, p game.Piece) *hboard {
+	h := new(hboard)
+	for i := range b {
+		for x := range b {
+			if b[x][i] != game.Empty {
+				h.origcols[i] |= (1 << uint16(x))
+			}
+		}
+		for y := range b {
+			if b[i][y] != game.Empty {
+				h.origrows[i] |= (1 << uint16(y))
+			}
+		}
+	}
+	h.rows, h.cols = h.origrows, h.origcols
+	return h
+}
+
+func (h *hboard) heuristic() int {
+	// maximize empty lines
+	var emptyLines int
+	for i := range h.rows {
+		if h.rows[i] == 0 {
+			emptyLines++
+		}
+		if h.cols[i] == 0 {
+			emptyLines++
 		}
 	}
 
 	// maximize space for "dangerous" pieces
-	cl15 := capacityLine1x5(b)
-	cl51 := capacityLine5x1(b)
-	csq3 := capacitySq3x3(b)
+	var cl15 int
+	for _, row := range h.rows {
+		cl15 += stride5Lookup[row]
+	}
+	var cl51 int
+	for _, col := range h.cols {
+		cl51 += stride5Lookup[col]
+	}
+	var csq3 int
+	for i := range h.rows[2:] {
+		// bitwise | 3 rows, then count strides of 3
+		csq3 += stride3Lookup[h.rows[i]|h.rows[i+1]|h.rows[i+2]]
+	}
+
+	// maximize contiguity
+	var contiguous, disparate int
+	for i := range h.rows[1:] {
+		// bitwise ^ 2 rows -- 0 means contiguous, 1 means disparate
+		ones := int(popcount(h.rows[i] ^ h.rows[i+1]))
+		contiguous += 10 - ones
+		disparate += ones
+	}
 
 	// apply weights
-	h := emptyLines*20 + contiguous*2 + disparate*-10 + cl15*20 + cl51*20 + csq3*50
-	return h
+	return emptyLines*20 + contiguous*2 + disparate*-10 + cl15*20 + cl51*20 + csq3*50
 }
 
-func capacityLine1x5(b *game.Board) (n int) {
-	for y := range b {
-		for x := 0; x < 10; x++ {
-			l := 0
-			for ; x < 10 && b.IsEmpty(x, y); x++ {
-				l++
-				if l >= 5 {
-					n++
-				}
-			}
-		}
-	}
-	return
-}
-
-func capacityLine5x1(b *game.Board) (n int) {
-	for x := range b {
-		for y := 0; y < 10; y++ {
-			l := 0
-			for ; y < 10 && b.IsEmpty(x, y); y++ {
-				l++
-				if l >= 5 {
-					n++
-				}
-			}
-		}
-	}
-	return
-}
-
-func capacitySq3x3(b *game.Board) (n int) {
-	for y := 0; y < 8; y++ {
-		for x := 0; x < 8; x++ {
-			if b.IsEmpty(x, y) && b.IsEmpty(x+1, y) && b.IsEmpty(x+2, y) &&
-				b.IsEmpty(x, y+1) && b.IsEmpty(x+1, y+1) && b.IsEmpty(x+2, y+1) &&
-				b.IsEmpty(x, y+2) && b.IsEmpty(x+1, y+2) && b.IsEmpty(x+2, y+2) {
-				n++
-			}
-		}
-	}
-	return
+func Heuristic(b *game.Board) int {
+	return makeHBoard(b, 0).heuristic()
 }
 
 type Move struct {
@@ -114,7 +158,7 @@ func BestMoves(b *game.Board, bag [3]game.Piece) [3]Move {
 		{bag[2], bag[0], bag[1]},
 		{bag[2], bag[1], bag[0]},
 	}
-	var scratch1, scratch2, scratch3 game.Board
+	var scratch1, scratch2 game.Board
 	bestPerm := perms[0]
 	var bestX, bestY [3]int
 	maxH := -1000000
@@ -137,16 +181,13 @@ func BestMoves(b *game.Board, bag [3]game.Piece) [3]Move {
 						if scratch2.Place(perm[1], x2, y2) <= 0 {
 							continue
 						}
+
+						hboard := makeHBoard(&scratch2, perm[2])
 						for x3 := 0; x3 <= 10-perm[2].Width(); x3++ {
 							for y3 := 0; y3 < 10-perm[2].Height(); y3++ {
-								if !scratch2.IsEmpty(x3, y3) {
+								if !hboard.place(perm[2], x3, y3) {
 									continue
-								}
-								scratch2.Copy(&scratch3)
-								if scratch3.Place(perm[2], x3, y3) <= 0 {
-									continue
-								}
-								if h := Heuristic(&scratch3); h > maxH {
+								} else if h := hboard.heuristic(); h > maxH {
 									maxH = h
 									bestPerm = perm
 									bestX[0], bestX[1], bestX[2], bestY[0], bestY[1], bestY[2] = x1, x2, x3, y1, y2, y3
