dcbd2372abc5eaba7f9bed13d74a426ed793519f
lukechampine <luke.champine@gmail.com>
2017-03-09 19:10:09 -0500

    hboard: precompute piece tables

--- ai/hboard.go
+++ ai/hboard.go
@@ -5,8 +5,8 @@ import "github.com/lukechampine/tenten/game"
 const fullLine = (uint16(1) << 10) - 1
 
 var popcounts [256]byte
-var stride3Lookup [1 << 10]int
-var stride5Lookup [1 << 10]int
+var stride3Lookup [1 << 16]int // only need 10 bits, but 16 eliminates the bounds check
+var stride5Lookup [1 << 16]int // only need 10 bits, but 16 eliminates the bounds check
 
 func init() {
 	for i := range popcounts {
@@ -19,7 +19,7 @@ func init() {
 		popcounts[i] = n
 	}
 
-	for u := range stride3Lookup {
+	for u := range stride3Lookup[:1<<10] {
 		for i := uint16(0); i < 10; i++ {
 			l := 0
 			for ; i < 10 && (u&(1<<i) == 0); i++ {
@@ -38,33 +38,37 @@ func init() {
 func popcount(x uint16) byte { return popcounts[byte(x)] + popcounts[byte(x>>8)] }
 
 type hboard struct {
-	origrows [10]uint16
-	origcols [10]uint16
-	rows     [10]uint16
-	cols     [10]uint16
+	origrows  [10]uint16
+	origcols  [10]uint16
+	rows      [10]uint16
+	cols      [10]uint16
+	piecerows []uint16
+	piececols []uint16
 }
 
 func (h *hboard) place(p game.Piece, x, y int) bool {
-	h.rows, h.cols = h.origrows, h.origcols
-	fullRows, fullCols := make([]byte, 0, 10), make([]byte, 0, 10)
-	for _, d := range p.Dots() {
-		// TODO: convert dots to row bits and use bits&row > 0 to check for collisions
-		dx, dy := uint16(d.X+x), uint16(d.Y+y)
-		if h.rows[dy]&(1<<dx) != 0 {
+	for i, prow := range h.piecerows {
+		if (prow<<uint16(x))&h.origrows[y+i] != 0 {
 			return false
-		}
-		h.rows[dy] |= 1 << dx
-		if h.rows[dy] == fullLine {
-			fullRows = append(fullRows, byte(dy))
-		}
-		if h.cols[dx]&(1<<dy) != 0 {
-			return false
-		}
-		h.cols[dx] |= 1 << dy
-		if h.cols[dx] == fullLine {
-			fullCols = append(fullCols, byte(dx))
 		}
 	}
+	h.rows, h.cols = h.origrows, h.origcols
+	fullRows, fullCols := make([]byte, 0, 10), make([]byte, 0, 10)
+	for i, prow := range h.piecerows {
+		dy := byte(y + i)
+		h.rows[dy] |= prow << uint16(x)
+		if h.rows[dy] == fullLine {
+			fullRows = append(fullRows, dy)
+		}
+	}
+	for i, pcol := range h.piececols {
+		dx := byte(x + i)
+		h.cols[dx] |= pcol << uint16(y)
+		if h.cols[dx] == fullLine {
+			fullCols = append(fullCols, dx)
+		}
+	}
+
 	for _, r := range fullRows {
 		h.rows[r] = 0
 		for i := range h.cols {
@@ -122,17 +126,27 @@ func (h *hboard) heuristic() int {
 func makeHBoard(b *game.Board, p game.Piece) *hboard {
 	h := new(hboard)
 	for i := range b {
-		for x := range b {
-			if b[x][i] != game.Empty {
-				h.origcols[i] |= (1 << uint16(x))
+		for y := range b {
+			if b[y][i] != game.Empty {
+				h.origcols[i] |= (1 << uint16(y))
 			}
 		}
-		for y := range b {
-			if b[i][y] != game.Empty {
-				h.origrows[i] |= (1 << uint16(y))
+		for x := range b {
+			if b[i][x] != game.Empty {
+				h.origrows[i] |= (1 << uint16(x))
 			}
 		}
 	}
 	h.rows, h.cols = h.origrows, h.origcols
+
+	h.piecerows = make([]uint16, p.Height())
+	for _, d := range p.Dots() {
+		h.piecerows[d.Y] |= (1 << uint16(d.X))
+	}
+	h.piececols = make([]uint16, p.Width())
+	for _, d := range p.Dots() {
+		h.piececols[d.X] |= (1 << uint16(d.Y))
+	}
+
 	return h
 }
